d678f0dab5ded3dd9f499cf964c0a465bf8070d7
Bharathy S. Parimalam <bparimalam@outlook.com>
2020-06-11 14:23:44 -0700

    v.0.0.4

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 addressbox = document.getElementById('addressbox');
+addressbar = document.getElementById('addressbar');
 mostusedlist = document.getElementById('mostused');
 browser = document.getElementById('browser');
 useragent = 'Mozilla/5.0 (Linux; Android 10; SM-G970U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.93 Mobile Safari/537.36';
@@ -15,56 +16,6 @@ try {
 	browser.contentWindow.navigator = Object.create(navigator, {
 		userAgent: { get: function() { return useragent;} }
 	});
-}
-
-function strip(url) {
-	if (url.slice(url.length-1, ) === '/') {
-		url = url.slice(0, url.length-1);
-	}
-	url = url.replace(/http:\/\/|https:\/\/|http:\/\/m\.|https:\/\/m\.|http:\/\/www\.|https:\/\/www\.|www\./i, '');
-	if (url.slice(0, 2) === 'm.') {
-		url =  url.slice(2, );
-	}
-	return  url;
-}
-function striptoname(url) {
-	url = url.slice(0, url.indexOf('/'));
-	url = url.slice(0, url.lastIndexOf('.'));
-	if (name.indexOf('.') !== -1) {
-		url = url.slice(url.lastIndexOf('.'), );
-	}
-	return url;
-}
-function loadthepage(url) {
-	url = strip(url);
-	addressbox.value = '';
-	// addressbox.placeholder = '';
-	if (url.indexOf('m.' !== -1)) {
-		browser.src = 'http://' + url;
-	} else {
-		browser.src = 'http://m.' + url;
-	}
-}
-
-if ((userpref !== null) && (userpref.recent !== '')) {
-	loadthepage(userpref.recent);
-} else {
-	loadthepage('en.m.wikipedia.org/wiki/Main_Page');
-}
-
-function loaduserpref() {
-	mostusedlist.innerHTML = '';
-	for (let i = 0; (i < 9) && (i < userpref.log.length ); i++) {
-		let opt = document.createElement('option');
-		let url = userpref.log[i]['url'];
-		opt.value = url;
-		opt.innerHTML = userpref.log[i]['name'];
-		mostusedlist.appendChild(opt);
-	}
-}
-
-function saveuserpref() {
-	window.localStorage.setItem(storagename, JSON.stringify(userpref));
 }
 
 if (userpref == null) { 
@@ -98,7 +49,60 @@ if (userpref == null) {
 			}
 		]
 	}
-} 
+}
+
+function strip(url) {
+	if (url.slice(url.length-1, ) === '/') {
+		url = url.slice(0, url.length-1);
+	}
+	url = url.replace(/http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/|www\./i, '');
+	return  url;
+}
+
+function striptoname(url) {
+	url = url.slice(0, url.indexOf('/'));
+	url = url.slice(0, url.lastIndexOf('.'));
+	if (url.indexOf('.') !== -1) {
+		url = url.slice(url.lastIndexOf('.')+1, );
+	}
+	return url;
+}
+
+function loadthepage(url) {
+	url = strip(url);
+	addressbox.value = '';
+	if (url.indexOf('.') === -1) {
+		browser.src = "http://google.com/search?btnI=1&sclient=mobile-gws-wiz-hp&q=" + url;
+		addressbox.placeholder = 'paste the first url here, for future access';
+	} else {
+		browser.src = 'http://' + url;
+		addressbox.placeholder = 'url or search';
+	}
+	log(url);
+}
+
+if ((userpref !== null) && (userpref.recent !== '')) {
+	loadthepage(userpref.recent);
+} else {
+	loadthepage('en.m.wikipedia.org/wiki/Main_Page');
+}
+
+function loaduserpref() {
+	mostusedlist.innerHTML = '';
+	for (let i = 0; (i < 9) && (i < userpref.log.length ); i++) {
+		let opt = document.createElement('option');
+		let url = userpref.log[i]['url'];
+		opt.value = url;
+		opt.innerHTML = userpref.log[i]['name'];
+		// opt.style.backgroundImageUrl = "http://${url}/favicon.ico";
+		mostusedlist.appendChild(opt);
+	}
+}
+
+function saveuserpref() {
+	window.localStorage.setItem(storagename, JSON.stringify(userpref));
+}
+
 loaduserpref();
 loadthepage(userpref.recent);
 // load user preferred conversions
@@ -112,6 +116,12 @@ function log (url) {
 	}
 	if (urlindex != userpref.log.length) {
 		userpref.log[urlindex].usecount += 1;
+	} else if (url.indexOf('.') === -1) {
+		// userpref.log.push({
+		// 	'name': 'search',
+		// 	'url' : url,
+		// 	'usecount'	: 1
+		// });
 	} else {
 		userpref.log.push({
 			'name': striptoname(url),
@@ -137,16 +147,45 @@ addressbox.addEventListener('change', event => {
 	loadthepage(addressbox.value);
 });
 
-browser.addEventListener('load', event => {
-	let url;
-	try {
-		url = browser.location.href;
-	} catch {
-		url = browser.src;
-	}
-	log(strip(url));
-});
+// addressbox.addEventListener('drop', event => {
+// 	event.preventDefault();
+// 	loadthepage(event.dataTransfer.getData('text'));
+// 	console.log('............................');
+// });
+// addressbox.addEventListener('dragover', event => {
+// 	event.preventDefault();
+// });
+// document.addEventListener('dragstart', event => {
+// 	ev.dataTransfer.setData("text", event.target.href);
+// });
 
-browser.addEventListener('error', event => {
-	browser.src = './error.html';
-});
+// document.addEventListener('click', event => {
+// 	let id = event.target.id;
+// 	console.log(id);
+// 	switch(id) {
+// 		case 'back': 
+// 			browser.contentWindow.history.back();
+// 			break;
+// 		case 'forward': 
+// 			browser.contentWindow.history.forward(); 
+// 			break;
+// 		case 'reload': 
+// 			browser.contentWindow.location.reload(); 
+// 			break;
+// 		case 'browser':
+
+// 	}
+// });
+// browser.addEventListener('load', event => {
+// 	let url;
+// 	try {
+// 		url = browser.location.href;
+// 	} catch {
+// 		url = browser.src;
+// 	}
+// 	log(strip(url));
+// });
+
+// browser.addEventListener('error', event => {
+// 	browser.src = './error.html';
+// });
