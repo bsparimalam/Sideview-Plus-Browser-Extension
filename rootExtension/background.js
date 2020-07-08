829a1aba1f581833f23e2579e6452d5c7fb6304e
Bharathy Subramanian Parimalam <bsubrama@sfu.ca>
2020-07-07 19:30:43 -0700

    v.1.0.4

--- rootExtension/background.js
+++ rootExtension/background.js
@@ -1,4 +1,88 @@
 
+// get nth index of a string in a string
+function nIndexOf(string, of, n) {
+	let index = -1;
+	for (let i=0; i < n; i++) {
+		index = string.indexOf(of, index+1);
+		if (index === -1) {
+			break;
+		}
+	}
+	return index;
+}
+// extract root address from url
+function url2root(url) {
+	let index = nIndexOf(url, '/', 3);
+	let root;
+	if (index === -1) { 
+		return url;
+	} else { 
+		return url.slice(0, nIndexOf(url, '/', 3));
+	}
+}
+function url2domain(url) {
+	url = url2root(url);
+	if (nIndexOf(url, '.', 2) === -1) {
+		return url;
+	} else {
+		let periods = url.split('.').length - 1;
+		return url.slice(0, nIndexOf(url, '/', 2) + 1) + url.slice(nIndexOf(url, '.', periods-1) + 1, );
+	}
+}
+function stripprotocol(string) {
+	return string.replace(/https:\/\/|http:\/\/|chrome:\/\/|edge:\/\//, '');
+}
+function gethost() {
+	let hostua = navigator.userAgent;
+	let temp = hostua.split('/');
+	temp = temp[temp.length-2];
+	temp = temp.split(' ')[1];
+	return temp;
+}
+// target mobile useragent
+function gettargetua() {
+	let temp = gethost();
+	if (temp === 'Edg') {
+		return 'Mozilla/5.0 (Linux; Android 10; HD1913) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36 Edg/45.3.4.4958';
+	} else if (temp === 'OPR') {
+		return 'Mozilla/5.0 (Linux; Android 10; VOG-L29) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36 OPR/55.2.2719';
+	} else if (temp === 'Firefox') {
+		return 'Mozilla/5.0 (Android 10; Mobile; rv:68.0) Gecko/68.0 Firefox/68.0';
+	} else {
+		return 'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36';
+	}
+}
+function widthcutoff() {
+	return (userpref.widthcutoff)*(window.screen.width);
+}
+function saveuserpref() {
+	window.localStorage.setItem(storagename, JSON.stringify(userpref));
+	chrome.runtime.sendMessage(userpref);
+	console.log(userpref);
+}
+function loaduserpref() {
+	return JSON.parse(window.localStorage.getItem(storagename));
+}
+function setuserpref() {
+	chrome.tabs.query({active:true, currentWindow:true}, tabs => {
+		let website = stripprotocol(url2root(tabs[0].url));
+		let domain = stripprotocol(url2domain(tabs[0].url));
+		websitename.innerText = website;
+		domainname.innerText = domain;
+		widthslider.value = userpref.widthcutoff;
+		if (userpref.disabledon.indexOf(domain) !== -1) {
+			websiteswitch.value = 0;
+			domainswitch.value = 0;
+		} else if (userpref.disabledon.indexOf(website) !== -1) {
+			websiteswitch.value = 0;
+			domainswitch.value = 1;
+		} else {
+			websiteswitch.value = 1;
+			domainswitch.value = 1;
+		}
+	});
+}
+
 windowtop = 0.05;
 windowleft = 0.55;
 windowwidth = 0.4;
@@ -12,24 +96,22 @@ screenwidth =  window.screen.width;
 screenheight = window.screen.height;
 tabwidth = undefined;
 taburl = undefined;
-tablist = {};
-// console.log(`host=${host}; targetua=${targetua}; screenwidth=${screenwidth}; screenheight=${screenheight}; widthcutoff=${widthcutoff}; userpref=${loaduserpref()}`);
-
-chrome.webNavigation.onCommitted.addListener(details => {
-	if (disableddomains().indexOf(url2domain2(details.url)) === -1) {
-		let tabid = details.tabId;
-		chrome.tabs.get(tabid, tab => {
-			let root = url2root(tab.url);
-			// console.log(`tabid=${tabid}; root=${root}; tablist[tabid]=${tablist[tabid]}`);
-			if (domainupgrade(tabid)) {
-			
-			} else if (!tablist[tabid]) {
-				chrome.tabs.reload(tabid, { "bypassCache": true });
-				tablist[tabid] = root;
-			}
-		});
+reflowlist = {};
+userpref = JSON.parse(window.localStorage.getItem(storagename));
+if (userpref === null) {
+	userpref = {
+		"disabledon": ['facebook.com', 'messenger.com', 'microsoft.com', 'whatsapp.com', 'devtools://devtools', "sfu.ca", 'youtube.com',  "extensions", "settings", "bookmarks", "history", "newtab" ],
+		"widthcutoff": 0.5
 	}
-	// console.log("userpreferences", loaduserpref());
+	window.localStorage.setItem(storagename, JSON.stringify(userpref));
+	console.log(`userpref=${userpref}`);
+}
+console.log(`host=${host}; targetua=${targetua}; screenwidth=${screenwidth}; screenheight=${screenheight}; widthcutoff=${widthcutoff}; userpref=${loaduserpref()}`);
+
+// receive user preferences
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	userpref = request;
+	console.log(`text received`, userpref);
 });
 
 // setting user agent on request
@@ -41,7 +123,8 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
 				tabwidth = tab.width;
 				taburl = tab.url;
 			});
-			if ((tabwidth < widthcutoff()) && (disableddomains().indexOf(url2domain2(taburl)) === -1)) {
+			// console.log(`request header: widthtest=${(tabwidth < widthcutoff())}; domaintest=${(userpref.disabledon.indexOf(stripprotocol(url2domain(taburl))) === -1)}; websitetest=${(userpref.disabledon.indexOf(stripprotocol(url2root(taburl))) === -1)}`);
+			if ((tabwidth < widthcutoff()) && (userpref.disabledon.indexOf(stripprotocol(url2domain(taburl))) === -1)  && (userpref.disabledon.indexOf(stripprotocol(url2root(taburl))) === -1)) {
 				for(let i = 0; i < details.requestHeaders.length; ++i) {
 					if (details.requestHeaders[i].name === 'User-Agent') {
 						details.requestHeaders[i].value = targetua;
@@ -49,7 +132,6 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
 					}
 				}
 			}
-			// console.log('request useragent: ', tabid, tabwidth, details);
 		}
 		return {requestHeaders: details.requestHeaders};
 	},
@@ -60,6 +142,78 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
 		'blocking', 'requestHeaders'
 	]
 );
+chrome.webNavigation.onCommitted.addListener(details => {
+	let taburl = details.url;
+	let tabid = details.tabId;
+	if ((tabid > -1) && (userpref.disabledon.indexOf(stripprotocol(url2domain(taburl))) === -1)  && (userpref.disabledon.indexOf(stripprotocol(url2root(taburl))) === -1)) {
+		chrome.tabs.get(tabid, tab => {
+			taburl = tab.url;
+			tabwidth = tab.width;
+			let root = url2root(taburl);
+			let domain = url2domain(taburl);
+			console.log(`request header: widthtest=${(tabwidth < widthcutoff())}; domaintest=${(userpref.disabledon.indexOf(stripprotocol(url2domain(taburl))) === -1)}; websitetest=${(userpref.disabledon.indexOf(stripprotocol(url2root(taburl))) === -1)}; mobilesitetest=${(taburl.match(/\/m\.|\.m\.|\/mobile\./) !== null)}; cachetest=${(!reflowlist[tabid] || ((reflowlist[tabid].status === 'ON') && (reflowlist[tabid].root === root) && (tabwidth > widthcutoff())) || ((reflowlist[tabid].status === 'OFF') && (reflowlist[tabid].root === root) && (tabwidth < widthcutoff())))}`);
+			//domain upgrade
+			if ((tabwidth > widthcutoff()) 
+				&& (root.match(/\/m\.|\.m\.|\/mobile\./) !== null)) {
+				let desktopurl = taburl.replace(/m\.|mobile\./, '');
+				chrome.tabs.update( tabid, { url: desktopurl } );
+			// cache bypass
+			} else if (!reflowlist[tabid] || ((reflowlist[tabid].status === 'ON') && (reflowlist[tabid].root === root) && (tabwidth > widthcutoff())) || ((reflowlist[tabid].status === 'OFF') && (reflowlist[tabid].root === root) && (tabwidth < widthcutoff()))) {
+				chrome.tabs.reload(tabid, { "bypassCache": true });
+			}
+			if (tabwidth < widthcutoff()) {
+				reflowlist[tabid] = {
+					"status": 'ON',
+					'domain': domain,
+					'root': root
+				}
+				chrome.browserAction.getBadgeText({'tabId': tabid}, result => {
+					if (result !== "ON") {
+						chrome.browserAction.setBadgeText({
+							"text": "ON",
+							"tabId": tabid
+						});
+					}
+				});
+			} else {
+				reflowlist[tabid] = {
+					"status": 'OFF',
+					'domain': domain,
+					'root': root
+				}
+				chrome.browserAction.getBadgeText({'tabId': tabid}, result => {
+					if (result !== "") {
+						chrome.browserAction.setBadgeText({
+							"text": "",
+							"tabId": tabid
+						});
+					}
+				});
+			}
+		});
+	}
+});
+// () => {
+// 	chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+// 		console.log('interval', tabs);
+// 		let tab = tabs[0];
+// 		tabid = tab.id;
+// 		if (tabid > -1) {
+// 			taburl = tab.url;
+// 			let domain = url2domain(taburl);
+// 			if (((reflowlist[tabid].status === 'ON') && (reflowlist[tabid].domain === domain) && (tabwidth > widthcutoff())) || ((reflowlist[tabid].status === 'OFF') && (reflowlist[tabid].domain === domain) && (tabwidth < widthcutoff()))) {
+// 				chrome.browserAction.getBadgeText({'tabId': tabid}, result => {
+// 					if (result !== "⭮") {
+// 						chrome.browserAction.setBadgeText({
+// 							"text": "⭮",
+// 							"tabId": tabid
+// 						});
+// 					}
+// 				});
+// 			}
+// 		}
+// 	});
+// }
 // CONTEXT MENU
 // define the options 
 let linkinpopup = {
@@ -108,87 +262,3 @@ chrome.contextMenus.onClicked.addListener((click) => {
 		chrome.windows.create(minibrowser);
 	}
 });
-// get nth index of a string in a string
-function nIndexOf(string, of, n) {
-	let index = -1;
-	for (let i=0; i < n; i++) {
-		index = string.indexOf(of, index+1);
-		if (index === -1) {
-			break;
-		}
-	}
-	return index;
-}
-// extract root address from url
-function url2root(url) {
-	let index = nIndexOf(url, '/', 3);
-	let root;
-	if (index === -1) { 
-		return url;
-	} else { 
-		return url.slice(0, nIndexOf(url, '/', 3));
-	}
-}
-function url2domain(url) {
-	url = url2root(url);
-	if (nIndexOf(url, '.', 2) === -1) {
-		return url;
-	} else {
-		let periods = url.split('.').length - 1;
-		return url.slice(0, nIndexOf(url, '/', 2) + 1) + url.slice(nIndexOf(url, '.', periods-1) + 1, );
-	}
-}
-function url2domain2(url) {
-	return url2domain(url).replace(/https:\/\/|http:\/\//, '');
-}
-function gethost() {
-	let hostua = navigator.userAgent;
-	let temp = hostua.split('/');
-	host = temp[temp.length-2];
-	return host.split(' ')[1];
-}
-// target mobile useragent
-function gettargetua() {
-	if (host === 'Edg') {
-		return 'Mozilla/5.0 (Linux; Android 10; HD1913) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36 Edg/45.3.4.4958';
-	} else if (host === 'OPR') {
-		return 'Mozilla/5.0 (Linux; Android 10; VOG-L29) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36 OPR/55.2.2719';
-	} else if (host === 'Firefox') {
-		return 'Mozilla/5.0 (Android 10; Mobile; rv:68.0) Gecko/68.0 Firefox/68.0';
-	} else {
-		return 'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36';
-	}
-}
-function domainupgrade(tabid) {
-	if (tabid > -1) {
-		chrome.tabs.get(tabid, tab => {
-			let url = tab.url;
-			if ((tab.width > widthcutoff()) 
-				&& (url.match(/\/m\.|\.m\.|\/mobile\./) !== null)) {
-				let desktopurl = url.replace(/m\.|mobile\./, '');
-				chrome.tabs.update( tabid, { url: desktopurl } );
-				// console.log('domainupgrade', tabid, url, desktopurl);
-				return true;
-			} else {
-				return false;
-			}
-		});
-	} else {
-		return false;
-	}
-}
-function loaduserpref() {
-	return JSON.parse(window.localStorage.getItem(storagename));
-}
-if (loaduserpref() === null) {
-	window.localStorage.setItem(storagename, JSON.stringify({
-		"disabledon": ['facebook.com', 'messenger.com', 'whatsapp.com'],
-		"widthcutoff": 0.5
-	}));
-}
-function widthcutoff() {
-	return (loaduserpref().widthcutoff)*(window.screen.width);
-}
-function disableddomains() {
-	return loaduserpref().disabledon;
-}
--- rootExtension/popup.js
+++ rootExtension/popup.js
@@ -1,38 +1,4 @@
-// globals
-domainname = document.getElementById('domain-name');
-thisswitch = document.getElementById('enable-disable');
-cutoffslider = document.getElementById('set-cut-off');
-storagename = 'reflow.1.0';
-userpref = loaduserpref();
 
-chrome.tabs.query({active:true, currentWindow:true}, tabs => {
-	let currentdomain = url2domain2(tabs[0].url);
-	domainname.innerHTML = currentdomain;
-	// console.log(userpref);
-	if (userpref.disabledon.indexOf(currentdomain) === -1) {
-		thisswitch.value = 1;
-	} else {
-		thisswitch.value = 0;
-	}
-	cutoffslider.value = userpref.widthcutoff;
-});
-
-thisswitch.addEventListener('change', event => {
-	let value = thisswitch.value;
-	if (value === "0") {
-		userpref.disabledon.push(domainname.innerHTML);
-		saveuserpref();
-	} else {
-		userpref.disabledon.splice(userpref.disabledon.indexOf(domainname.innerHTML), 1);
-		saveuserpref();
-	}
-	// console.log(userpref);
-});
-cutoffslider.addEventListener('change', event => {
-	userpref.widthcutoff = parseFloat(cutoffslider.value);
-	saveuserpref();
-	// console.log(userpref);
-});
 // get nth index of a string in a string
 function nIndexOf(string, of, n) {
 	let index = -1;
@@ -63,19 +29,120 @@ function url2domain(url) {
 		return url.slice(0, nIndexOf(url, '/', 2) + 1) + url.slice(nIndexOf(url, '.', periods-1) + 1, );
 	}
 }
-function url2domain2(url) {
-	return url2domain(url).replace(/https:\/\/|http:\/\//, '');
+function stripprotocol(string) {
+	return string.replace(/https:\/\/|http:\/\/|chrome:\/\/|edge:\/\//, '');
+}
+function gethost() {
+	let hostua = navigator.userAgent;
+	let temp = hostua.split('/');
+	temp = temp[temp.length-2];
+	temp = temp.split(' ')[1];
+	return temp;
+}
+// target mobile useragent
+function gettargetua() {
+	let temp = gethost();
+	if (temp === 'Edg') {
+		return 'Mozilla/5.0 (Linux; Android 10; HD1913) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36 Edg/45.3.4.4958';
+	} else if (temp === 'OPR') {
+		return 'Mozilla/5.0 (Linux; Android 10; VOG-L29) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36 OPR/55.2.2719';
+	} else if (temp === 'Firefox') {
+		return 'Mozilla/5.0 (Android 10; Mobile; rv:68.0) Gecko/68.0 Firefox/68.0';
+	} else {
+		return 'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36';
+	}
+}
+function widthcutoff() {
+	return (userpref.widthcutoff)*(window.screen.width);
 }
 function saveuserpref() {
 	window.localStorage.setItem(storagename, JSON.stringify(userpref));
-	userpref = loaduserpref();
+	chrome.runtime.sendMessage(userpref);
+	console.log(userpref);
 }
 function loaduserpref() {
 	return JSON.parse(window.localStorage.getItem(storagename));
 }
-function widthcutoff() {
-	return (loaduserpref().widthcutoff)*(window.screen.width);
+function setuserpref() {
+	chrome.tabs.query({active:true, currentWindow:true}, tabs => {
+		let website = stripprotocol(url2root(tabs[0].url));
+		let domain = stripprotocol(url2domain(tabs[0].url));
+		websitename.innerText = website;
+		domainname.innerText = domain;
+		widthslider.value = userpref.widthcutoff;
+		if (userpref.disabledon.indexOf(domain) !== -1) {
+			websiteswitch.value = 0;
+			domainswitch.value = 0;
+		} else if (userpref.disabledon.indexOf(website) !== -1) {
+			websiteswitch.value = 0;
+			domainswitch.value = 1;
+		} else {
+			websiteswitch.value = 1;
+			domainswitch.value = 1;
+		}
+		console.log(userpref);
+	});
 }
-function disableddomains() {
-	return loaduserpref().disabledon;
+// globals
+websitename = document.getElementById('website');
+host = gethost();
+domainname =  document.getElementById('domain');
+websiteswitch = document.getElementById('website-switch');
+domainswitch = document.getElementById('domain-switch');
+widthslider = document.getElementById('width-slider');
+schematic = document.getElementById('schematic');
+reloadbutton = document.getElementById('reload');
+storagename = 'reflow.1.0';
+userpref = loaduserpref();
+setuserpref();
+if (host === "Firefox") {
+	reloadbutton.innerText = 'Reload ( Ctrl + Shift + R)';
 }
+document.addEventListener('change', event => {
+	switch(event.target.id) {
+		case 'width-slider':
+			userpref.widthcutoff = parseFloat(event.target.value);
+			break;
+		case 'website-switch':
+			if (event.target.value === '0') {
+				userpref.disabledon.push(websitename.innerText);
+			} else {
+				while (userpref.disabledon.indexOf(websitename.innerText) !== -1) {
+					userpref.disabledon.splice(userpref.disabledon.indexOf(websitename.innerText), 1);
+				}
+				while (userpref.disabledon.indexOf(domainname.innerText) !== -1) {
+					userpref.disabledon.splice(userpref.disabledon.indexOf(domainname.innerText), 1);
+				}
+			} 
+			break;
+		case 'domain-switch':
+			if (event.target.value === '0') {
+				userpref.disabledon.push(domainname.innerText);
+			} else {
+				while (userpref.disabledon.indexOf(domainname.innerText) !== -1) {
+					userpref.disabledon.splice(userpref.disabledon.indexOf(domainname.innerText), 1);
+				}
+			}
+			break;
+	}
+	saveuserpref();
+	setuserpref();
+});
+reloadbutton.addEventListener('click', event => {
+	chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+		let tab = tabs[0];
+		let tabid = tab.id;
+		let taburl = tab.url;
+		let root = url2root(taburl);
+		//domain upgrade
+		if ((root.match(/\/m\.|\.m\.|\/mobile\./) !== null) && (tabid > -1) && ((userpref.disabledon.indexOf(stripprotocol(url2domain(taburl))) !== -1)  || (userpref.disabledon.indexOf(stripprotocol(url2root(taburl))))
+					!== -1)) {
+			let desktopurl = taburl.replace(/m\.|mobile\./, '');
+			chrome.tabs.update( tabid, { url: desktopurl } );
+		// cache bypass
+		} else if (tabid > -1) {
+			chrome.tabs.reload(tabid, { "bypassCache": true });
+		}
+	});
+	window.close();
+});
