0d0fc9ffe2f074246f361f989e16080a6a1803df
root <bparimalam@outlook.com>
2020-07-21 12:13:48 -0700

    v.1.1.0

--- popup.js
+++ popup.js
@@ -1,4 +1,51 @@
+// globals
+storagename = 'reflow.1.0';
+userpref = JSON.parse(window.localStorage.getItem(storagename));
 
+// get nth index of a string in a string
+function nIndexOf(string, of, n) {
+	let index = -1;
+	for (let i=0; i < n; i++) {
+		index = string.indexOf(of, index+1);
+		if (index === -1) {
+			break;
+		}
+	}
+	return index;
+}
+// extract page address from url
+function url2page(url) {
+	let index = stripprotocol(url).lastIndexOf('/');
+	if (index === -1) {
+		return url;
+	} else {
+		return url.slice(0, url.lastIndexOf('/'));
+	}
+}
+// extract root address from url
+function url2root(url) {
+	let index = nIndexOf(url, '/', 3);
+	let root;
+	if (index === -1) { 
+		return url;
+	} else { 
+		return url.slice(0, nIndexOf(url, '/', 3));
+	}
+}
+function url2domain(url) {
+	url = url2root(url);
+	if (nIndexOf(url, '.', 2) === -1) {
+		return url;
+	} else {
+		let periods = url.split('.').length - 1;
+		return url.slice(0, nIndexOf(url, '/', 2) + 1) + url.slice(nIndexOf(url, '.', periods-1) + 1, );
+	}
+}
+function stripprotocol(string) {
+	return string.replace(/https:\/\/|http:\/\//, '');
+}
+
+//////////////////////////////////////////////////////////////////////////////
 function setuserpref() {
 	chrome.tabs.query({active:true, currentWindow:true}, tabs => {
 		let page = stripprotocol(url2page(tabs[0].url));
@@ -19,22 +66,36 @@ function setuserpref() {
 			websitename.innerText = 'subdomain';
 			websitename.style.color = 'var(--disabled)';
 		}
-		if (userpref.disabledon.indexOf(domain) !== -1) {
+		if (domain.indexOf('.') > -1) {
+			if (userpref.disabledon.indexOf(domain) !== -1) {
+				pageswitch.value = 0;
+				websiteswitch.value = 0;
+				domainswitch.value = 0;
+			} else if (userpref.disabledon.indexOf(website) !== -1) {
+				pageswitch.value = 0;
+				websiteswitch.value = 0;
+				domainswitch.value = 1;
+			} else if (userpref.disabledon.indexOf(page) !== -1) {
+				pageswitch.value = 0;
+				websiteswitch.value = 1;
+				domainswitch.value = 1;		
+			} else {
+				pageswitch.value = 1;
+				websiteswitch.value = 1;
+				domainswitch.value = 1;	
+			}
+		} else {
+			pageswitch.disabled = true;
+			websiteswitch.disabled = true;
+			domainswitch.disabled = true;
+			websitename.innerText = 'subdomain';
+			domainname.innerText = 'domain';
+			pagename.style.color = 'var(--disabled)';
+			websitename.style.color = 'var(--disabled)';
+			domainname.style.color = 'var(--disabled)';
 			pageswitch.value = 0;
 			websiteswitch.value = 0;
 			domainswitch.value = 0;
-		} else if (userpref.disabledon.indexOf(website) !== -1) {
-			pageswitch.value = 0;
-			websiteswitch.value = 0;
-			domainswitch.value = 1;
-		} else if (userpref.disabledon.indexOf(page) !== -1) {
-			pageswitch.value = 0;
-			websiteswitch.value = 1;
-			domainswitch.value = 1;		
-		} else {
-			pageswitch.value = 1;
-			websiteswitch.value = 1;
-			domainswitch.value = 1;	
 		}
 		bugreport.href = `mailto:support@bhar.app?subject=Reflow - bug report - ${website}&body=Hi, I found an issue with the extension. Issue description: `;
 		usage.innerText = Math.floor(userpref.totalreflows) + ' pages reflowed';
@@ -102,20 +163,7 @@ document.addEventListener('change', event => {
 });
 
 reloadbutton.addEventListener('click', event => {
-	chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-		let tab = tabs[0];
-		let tabid = tab.id;
-		let taburl = tab.url;
-		let root = url2root(taburl);
-		//domain upgrade
-		if ((root.match(/\/m\.|\.m\.|\/mobile\./) !== null) && (tabid > -1)) {
-			let desktopurl = taburl.replace(/m\.|mobile\./, '');
-			chrome.tabs.update( tabid, { url: desktopurl } );
-		// cache bypass
-		} else if (tabid > -1) {
-			chrome.tabs.reload(tabid, { "bypassCache": true });
-		}
-	});
+	chrome.runtime.sendMessage('reload');
 	window.close();
 });	
 
--- reflow.js
+++ reflow.js
@@ -1,3 +1,52 @@
+// globals
+storagename = 'reflow.1.0';
+userpref = JSON.parse(window.localStorage.getItem(storagename));
+
+// get nth index of a string in a string
+function nIndexOf(string, of, n) {
+	let index = -1;
+	for (let i=0; i < n; i++) {
+		index = string.indexOf(of, index+1);
+		if (index === -1) {
+			break;
+		}
+	}
+	return index;
+}
+// extract page address from url
+function url2page(url) {
+	let index = stripprotocol(url).lastIndexOf('/');
+	if (index === -1) {
+		return url;
+	} else {
+		return url.slice(0, url.lastIndexOf('/'));
+	}
+}
+// extract root address from url
+function url2root(url) {
+	let index = nIndexOf(url, '/', 3);
+	let root;
+	if (index === -1) { 
+		return url;
+	} else { 
+		return url.slice(0, nIndexOf(url, '/', 3));
+	}
+}
+function url2domain(url) {
+	url = url2root(url);
+	if (nIndexOf(url, '.', 2) === -1) {
+		return url;
+	} else {
+		let periods = url.split('.').length - 1;
+		return url.slice(0, nIndexOf(url, '/', 2) + 1) + url.slice(nIndexOf(url, '.', periods-1) + 1, );
+	}
+}
+function stripprotocol(string) {
+	return string.replace(/https:\/\/|http:\/\//, '');
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
 // functions
 function gethost() {
 	let hostua = navigator.userAgent;
@@ -39,21 +88,15 @@ if (userpref === null) {
 			'height': Math.round(screenheight*0.90)
 		},
 		"disabledon": [
-			'bookmarks',
-			'downloads',
-			'extensions',
-			'history',
-			'newtab',
-			'settings',
 			'apple.com',
 			'adobe.com',
 			'bbc.com',
+			'bhar.app',
 			'cloudflare.com',
 			'cnet.com',
 			'cnn.com',
 			'creativecommons.org',
 			'dailymotion.com',
-			'devtools://devtools',
 			'europa.eu',
 			'facebook.com',
 			'abcnews.go.com',
@@ -70,7 +113,6 @@ if (userpref === null) {
 			'mozilla.org',
 			'nytimes.com',
 			'paypal.com',
-			'sfu.ca',
 			'whatsapp.com',
 			'youtube.com'
 		],
@@ -95,7 +137,8 @@ chrome.webRequest.onBeforeSendHeaders.addListener(details => {
 				taburl = tab.url;
 			});
 		}
-		if ((tabid > -1) && (tabwidth < userpref.widthcutoff*screenwidth) && (userpref.disabledon.indexOf(stripprotocol(url2domain(taburl))) === -1)  && (userpref.disabledon.indexOf(stripprotocol(url2root(taburl))) === -1) && (userpref.disabledon.indexOf(stripprotocol(url2page(taburl))) === -1)) {
+		let domain = url2domain(taburl);
+		if ((tabid > -1) && (domain.indexOf('.') > -1) && (tabwidth < userpref.widthcutoff*screenwidth) && (userpref.disabledon.indexOf(stripprotocol(domain)) === -1)  && (userpref.disabledon.indexOf(stripprotocol(url2root(taburl))) === -1) && (userpref.disabledon.indexOf(stripprotocol(url2page(taburl))) === -1)) {
 			for(let i = 0; i < details.requestHeaders.length; ++i) {
 				if (details.requestHeaders[i].name === 'User-Agent') {
 					details.requestHeaders[i].value = targetua;
@@ -113,65 +156,63 @@ chrome.webRequest.onBeforeSendHeaders.addListener(details => {
 	]
 );
 
-chrome.webNavigation.onCommitted.addListener(details => {
-	console.log(reflowlist);
-	let taburl = details.url;
-	let tabid = details.tabId;
-	if ((tabid > -1) && (details.parentFrameId === -1)) {
-		chrome.tabs.get(tabid, tab => {
-			console.log(tab);
-			taburl = tab.url;
-			tabwidth = tab.width;
-			let root = url2root(taburl);
-			let domain = url2domain(taburl);
-			if ((userpref.disabledon.indexOf(stripprotocol(url2domain(taburl))) === -1)  && (userpref.disabledon.indexOf(stripprotocol(url2root(taburl))) === -1) && (userpref.disabledon.indexOf(stripprotocol(url2page(taburl))) === -1)) {
-				//domain upgrade
-				if ((tabwidth > userpref.widthcutoff*screenwidth) 
-					&& (root.match(/\/m\.|\.m\.|\/mobile\./) !== null)) {
-					let desktopurl = taburl.replace(/m\.|mobile\./, '');
-					chrome.tabs.update( tabid, { url: desktopurl } );
-				// cache bypass
-				} else if (!reflowlist[tabid] || (reflowlist[tabid].root !== root) || ((reflowlist[tabid].status === 'ON') && (tabwidth > userpref.widthcutoff*screenwidth)) || ((reflowlist[tabid].status === 'OFF') && (tabwidth < userpref.widthcutoff*screenwidth))) {
-					chrome.tabs.reload(tabid, { "bypassCache": true });
-					if (tabwidth < userpref.widthcutoff*screenwidth) {
-						userpref.totalreflows -= 1;
-					}
-				}
-				if (tabwidth < userpref.widthcutoff*screenwidth) {
-					userpref.totalreflows += 1;
-					window.localStorage.setItem(storagename, JSON.stringify(userpref));
-					reflowlist[tabid] = {
-						"status": 'ON',
-						'domain': domain,
-						'root': root
-					}
-					chrome.browserAction.getBadgeText({'tabId': tabid}, result => {
-						if (result !== "ON") {
-							chrome.browserAction.setBadgeText({
-								"text": "ON",
-								"tabId": tabid
-							});
-						}
-					});
-				} else {
-					reflowlist[tabid] = {
-						"status": 'OFF',
-						'domain': domain,
-						'root': root
-					}
-					chrome.browserAction.getBadgeText({'tabId': tabid}, result => {
-						if (result !== "") {
-							chrome.browserAction.setBadgeText({
-								"text": "",
-								"tabId": tabid
-							});
-						}
-					});
-				}
-			} 
-		});
-	}
-});
+// chrome.webNavigation.onCommitted.addListener(details => {
+// 	let taburl = details.url;
+// 	let tabid = details.tabId;
+// 	if ((tabid > -1) && (details.parentFrameId === -1)) {
+// 		chrome.tabs.get(tabid, tab => {
+// 			taburl = tab.url;
+// 			tabwidth = tab.width;
+// 			let root = url2root(taburl);
+// 			let domain = url2domain(taburl);
+// 			if ((domain.indexOf('.') > -1) && (userpref.disabledon.indexOf(stripprotocol(url2domain(taburl))) === -1) && (userpref.disabledon.indexOf(stripprotocol(url2root(taburl))) === -1) && (userpref.disabledon.indexOf(stripprotocol(url2page(taburl))) === -1)) {
+// 				//domain upgrade
+// 				if ((tabwidth > userpref.widthcutoff*screenwidth) 
+// 					&& (root.match(/\/m\.|\.m\.|\/mobile\./) !== null)) {
+// 					let desktopurl = taburl.replace(/m\.|mobile\./, '');
+// 					chrome.tabs.update( tabid, { url: desktopurl } );
+// 				// cache bypass
+// 				} else if (!reflowlist[tabid] || (reflowlist[tabid].root !== root) || ((reflowlist[tabid].status === 'ON') && (tabwidth > userpref.widthcutoff*screenwidth)) || ((reflowlist[tabid].status === 'OFF') && (tabwidth < userpref.widthcutoff*screenwidth))) {
+// 					chrome.tabs.reload(tabid, { "bypassCache": true });
+// 					if (tabwidth < userpref.widthcutoff*screenwidth) {
+// 						userpref.totalreflows -= 1;
+// 					}
+// 				}
+// 				if (tabwidth < userpref.widthcutoff*screenwidth) {
+// 					userpref.totalreflows += 1;
+// 					window.localStorage.setItem(storagename, JSON.stringify(userpref));
+// 					reflowlist[tabid] = {
+// 						"status": 'ON',
+// 						'domain': domain,
+// 						'root': root
+// 					}
+// 					chrome.browserAction.getBadgeText({'tabId': tabid}, result => {
+// 						if (result !== "ON") {
+// 							chrome.browserAction.setBadgeText({
+// 								"text": "ON",
+// 								"tabId": tabid
+// 							});
+// 						}
+// 					});
+// 				} else {
+// 					reflowlist[tabid] = {
+// 						"status": 'OFF',
+// 						'domain': domain,
+// 						'root': root
+// 					}
+// 					chrome.browserAction.getBadgeText({'tabId': tabid}, result => {
+// 						if (result !== "") {
+// 							chrome.browserAction.setBadgeText({
+// 								"text": "",
+// 								"tabId": tabid
+// 							});
+// 						}
+// 					});
+// 				}
+// 			} 
+// 		});
+// 	}
+// });
 
 // CONTEXT MENU
 // define the options 
@@ -197,7 +238,6 @@ chrome.contextMenus.create(linkinpopup);
 chrome.contextMenus.create(newpopup);
 // define the window parameters
 let minibrowser = {
-	"type": "popup",
 	"state": "normal"
 };
 if (host === 'Firefox') {
@@ -213,36 +253,61 @@ chrome.contextMenus.onClicked.addListener((click) => {
 	minibrowser.width = userpref.miniwindow.width;
 	minibrowser.height = userpref.miniwindow.height;
 	if ((click.menuItemId ==='searchinpopup') && click.selectionText) {
+		minibrowser.type = 'popup';
 		minibrowser.url = 'https://google.com/search?q=' + click.selectionText;
 		chrome.windows.create(minibrowser);
 	} else if ((click.menuItemId === 'linkinpopup') && click.linkUrl) {
+		minibrowser.type = 'popup';
 		minibrowser.url = click.linkUrl;
 		chrome.windows.create(minibrowser);
 	} else if ((click.menuItemId === 'newpopup')) {
 		minibrowser.type = "normal";
-		minibrowser.url = 'chrome://newtab';
+		if (host === 'Firefox') {
+			minibrowser.url = 'https://google.com/';
+		} else {
+			minibrowser.url = 'chrome://newtab';
+		}
 		chrome.windows.create(minibrowser);
 	}
-	chrome.windows.getCurrent(details => {
-		setTimeout(() => {
-			try {
-				chrome.windows.get(details.id, details => {
-					if (details) {
-						userpref.miniwindow.top = details.top;
-						userpref.miniwindow.left = details.left;
-						userpref.miniwindow.width = details.width;
-						userpref.miniwindow.height = details.height;
-						window.localStorage.setItem(storagename, JSON.stringify(userpref));
-					}
-				});
-			} catch { }
-		}, 5000);
-	});
+	if (host !== 'Firefox') {
+		chrome.windows.getCurrent(details => {
+			setTimeout(() => {
+				try {
+					chrome.windows.get(details.id, details => {
+						if (details) {
+							userpref.miniwindow.top = details.top;
+							userpref.miniwindow.left = details.left;
+							userpref.miniwindow.width = details.width;
+							userpref.miniwindow.height = details.height;
+							window.localStorage.setItem(storagename, JSON.stringify(userpref));
+						}
+					});
+				} catch {}
+			}, 5000);
+		});
+	}
 });
 
 // receive user preferences
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	userpref = request;
-	window.localStorage.setItem(storagename, JSON.stringify(userpref));
-	console.log(`text received`, userpref);
+	if (request === 'reload') {
+		chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+			let tab = tabs[0];
+			let tabid = tab.id;
+			let taburl = tab.url;
+			let root = url2root(taburl);
+			//domain upgrade
+			if ((root.match(/\/m\.|\.m\.|\/mobile\./) !== null) && (tabid > -1)) {
+				console.log('root upgrade true', tabid);
+				let desktopurl = taburl.replace(/m\.|mobile\./, '');
+				chrome.tabs.update( tabid, { url: desktopurl } );
+			// cache bypass
+			} else if (tabid > -1) {
+				chrome.tabs.reload(tabid, { "bypassCache": true });
+			}
+		});
+	} else {
+		userpref = request;
+		window.localStorage.setItem(storagename, JSON.stringify(userpref));
+	}
 });
